d713a75cacb55fe82bfbca16832b1f2b8ee50d90
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2025-12-07 22:04:08 +0600

    Js Data Type explain

---
+++ 01_Basics/02-dataType.js
@@ -0,0 +1,11 @@
+/*
+Javascript Data Types 7 items 
+lets start,,,
+*/
+const name = "md selim ahmed" //String
+let ages = 30; // Number
+isStudent = true; // Boolean /true
+newStudent; // Undefined
+// Null // standalone value
+// symbol => Unique data type
+
