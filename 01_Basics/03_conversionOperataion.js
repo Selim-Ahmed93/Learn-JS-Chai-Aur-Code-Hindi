2cd0b190cf25f886af01fc16fc673f13408ed12a
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2025-12-08 22:20:36 +0600

    operation js concepts is done

--- 01_Basics/03_conversionOperataion.js
+++ 01_Basics/03_conversionOperataion.js
@@ -12,4 +12,31 @@ console.log(typeof valueInNumber)
 //"34abc" => NaN this  not a number
 // true => 1
 // false => 0
+//****************Operations********/
+let value = 3;
+let netValue = - value;
+// console.log(netValue)
+let str1 =  "md selim"
+let str2 = 'ahmed'
+let str3 = str1+str2;
+// console.log(str3)
+
+// console.log("1" +2)
+// console.log(1+"2")
+// console.log(1+5*8+"7");
+// console.log(true);
+// console.log(1+"3");
+// console.log(+"");
+
+// let gameCounter = 100;
+// --gameCounter;
+// console.log(gameCounter);
+
+let num1, num2,num3;
+ num1 = num2=num3 = 2 + 2;
+ console.log(num1);
+ 
+
+
+
 
