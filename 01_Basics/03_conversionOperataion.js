1abcdbf683c8ac0a9b7d27228a7d8566a9323fff
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2025-12-08 20:59:40 +0600

    Conversion of dataType

---
+++ 01_Basics/03_conversionOperataion.js
@@ -0,0 +1,15 @@
+let score = "33";
+console.log(typeof(score))
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber)
+
+console.log(typeof valueInNumber)
+
+// 34 => number
+//"34" => this is string value
+//"34abc" => NaN this  not a number
+// true => 1
+// false => 0
+
