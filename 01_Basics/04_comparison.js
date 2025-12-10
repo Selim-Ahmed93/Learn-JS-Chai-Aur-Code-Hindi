8f8bedbffe0d8827bf6cfb9becb0d022638125a0
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2025-12-10 22:01:28 +0600

    comparison operator done

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,24 @@
+console.log(2>6);//result False 2small 6 is big number 
+console.log(6<8);//result true 6small value 8 big value
+console.log(8>=10);// result false 10 is big 8 small and not equal
+console.log(10<=12);// true 12 is big and 10 is small and not equal
+console.log(20==18);//this is equal sign result is false 20 and 18 not equal
+console.log(23 != 7);
+console.log(8 != 8);
+
+//this code dont use production code
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+// console.log(undefined = 0);
+// console.log("8" === 8);
+
+
+
+
+
+
+
+
+
