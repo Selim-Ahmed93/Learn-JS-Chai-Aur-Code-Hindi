5a0695144df1709011d7d648ac5ef76324ba5d56
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2025-12-15 21:43:22 +0600

    Stack and heap memory concept done

---
+++ 01_Basics/06_Stack_HeapMemoryin_JS.js
@@ -0,0 +1,22 @@
+// Stack memory received only Primitive Data Type..
+// Heap Memory received  only Non Primitive Data ...
+
+let Email = "mdselimahmed6467@gmail.com";
+let myEmail = Email;
+console.log(myEmail);
+
+// Email  value to refer to myEmail variable
+
+const student ={
+      name:"md Ayman Ahmed",
+      id: 1234,
+      PassWord:"selim@hs",
+}
+
+ console.log(student);
+ 
+const myStudent =(student)
+console.log(myStudent.name);
+
+
+
