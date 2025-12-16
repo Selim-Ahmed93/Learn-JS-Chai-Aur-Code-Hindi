8e69fb4015e700da77c7af545f9dc9e8d6a7fc2c
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2025-12-16 09:28:15 +0600

    javascript string method done

---
+++ 01_Basics/07_String.js
@@ -0,0 +1,27 @@
+let name = "selim";
+countRepo = 50;
+console.log(name + "hello how are you");
+console.log(`i am ${name} Ahmed my passion is coding ${countRepo} `);
+
+const gameName = new String("hello js");
+// console.log(gameName);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(1));
+console.log(gameName.indexOf("e"));
+
+const newString = "    conversion  ";
+console.log(newString);
+console.log(newString.trim());
+
+const url ="https://hitesh.com/hitesh%30chowdory";
+console.log(url);
+
+console.log(url.replace("%30","-"));
+console.log(url.includes("hitesh"));
+console.log(url.includes("ayman"));
+
+
+
+
