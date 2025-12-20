9ab8fc3489488afbf5827470061d48a0b69806e2
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2025-12-20 22:12:32 +0600

    Arrays methods

---
+++ 01_Basics/11_Arrays_function.js
@@ -0,0 +1,34 @@
+const movies = ["thor","harry","potter"];
+const new_Movie= ["superman","spider","Raa"];
+
+// movies.push(new_Movie);
+// const newMovies = movies.concat(new_Movie);
+
+// console.log(newMovies)
+
+// this is a (... spread operator)//
+
+// const all_movies = [...movies,...new_Movie];
+
+// console.log(all_movies)
+
+const numbers =[1,2,4,[5,6,7],[8,9,5],98,55];
+
+// // console.log(numbers);
+// const All_Numbers =numbers.flat(Infinity)
+// console.log(All_Numbers);
+
+// console.log(Array.isArray('hitesh'));
+// console.log(Array.from('hitesh'));
+
+const score1 = 100;
+const score2 = 200;
+const score3 = 300;
+
+const All_Score = Array.of(score1,score2,score3);
+
+console.log(All_Score);
+
+      
+
+
