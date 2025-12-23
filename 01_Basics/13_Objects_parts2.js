ef34dd76e2ebe6968169f8d7301ca8bd9897fb69
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2025-12-23 19:47:40 +0600

    objects part 2 done

--- 01_Basics/13_Objects_parts2.js
+++ 01_Basics/13_Objects_parts2.js
@@ -1,11 +1,11 @@
-// // const tenderId = new Object();
+// const tenderId = new Object();
 
-// const tenderId = {};
+const tenderId = {};
 
-// tenderId.id ='hello world';
-// tenderId.name = 'Md selim ahmed';
-// tenderId.password = '12345dfi';
-// tenderId.loggedIn = false;
+tenderId.id ='hello world';
+tenderId.name = 'Md selim ahmed';
+tenderId.password = '12345dfi';
+tenderId.loggedIn = false;
 
 // // tenderId.greeting = function(){
 // //     console.log(`my name is a ${this.name}, And my password, ${this.password}`);
@@ -17,3 +17,32 @@
 //              return value;
 // }
 
+// const obj1 ={n:'hero',h:'how'};
+// const obj2 ={b:'Hello',d:'howare'};
+
+// const AllObj = {...obj1,...obj2}
+
+// // const AllObj =Object.assign({},obj1,obj2)
+
+
+// console.log(AllObj);
+
+// console.log(AllObj === obj2);
+
+//  const user1={
+//      id:233,
+//      name:"md selim ahmed",
+//     }
+const userInfo =[
+//   user1,
+    {
+        id:545,
+        name:"Shapla Ahmed"
+    },
+    {
+        id:678,
+        name:"Ayman Ahmed"
+    }
+]
+
+console.log(Object.values(tenderId))
