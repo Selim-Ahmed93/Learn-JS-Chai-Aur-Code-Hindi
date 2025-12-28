2e4d58c52f709733e2c13c5cab6a08c280c84e2c
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2025-12-28 20:54:25 +0600

    function and simple defination

---
+++ 01_Basics/15_function.js
@@ -0,0 +1,39 @@
+// console.log('s');
+// console.log('e');
+// console.log('l');
+// console.log('i');
+// console.log('m');
+
+// function sayName (){
+//     console.log('s');
+// console.log('e');
+// console.log('l');
+// console.log('i');
+// console.log('m');
+// };
+
+// sayName();
+
+// const numbers =1234;
+// const numbers2 =3456;
+
+ 
+
+// function addTwoNumber(num1,num2){
+//     let Result = num1+num2;
+//         console.log("hitesh");
+//         return Result;
+        
+//     }
+   
+// const Result = (addTwoNumber(5,7));
+
+// console.log("Result:" , Result);
+
+
+function loginUserMessage(userName){
+   return`${userName} just logged in`;
+}
+
+ console.log(loginUserMessage("Ayman"));
+ 
