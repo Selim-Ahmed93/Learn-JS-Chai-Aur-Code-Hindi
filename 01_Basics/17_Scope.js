83de99ca146da98c4099c14f1b48a54101788b2e
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2026-01-10 20:15:43 +0600

    Javasceript Global Scope And Block scope

---
+++ 01_Basics/17_Scope.js
@@ -0,0 +1,19 @@
+ let a = 500;
+//  const b = 20;
+//  var c = 30;
+
+
+if(true){
+let a = 10;
+const b = 20;
+var c = 30;
+console.log(b);
+console.log(`Inner value :${a}`);
+};
+
+console.log(`This Is Global variable output:${a}`);
+
+
+// console.log(a);
+// console.log(c);
+
