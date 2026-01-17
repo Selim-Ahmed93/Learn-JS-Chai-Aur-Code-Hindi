6300810db664d4d615701963b9dd81e30996c9f9
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2026-01-17 21:45:34 +0600

    comparision operator done

--- 01_Basics/19_IIFE.js
+++ 01_Basics/19_IIFE.js
@@ -1,17 +1,53 @@
-(function chai(){
-    console.log('DB CONNECTED');
-})();
+// (function chai(){
+//     console.log('DB CONNECTED');
+// })();
 
-// Arrow Function With IIFE//
+// // Arrow Function With IIFE//
 
-(() =>{
-    console.log('DB Connect Two');
+// (() =>{
+//     console.log('DB Connect Two');
     
-})();
+// })();
 
-((name) =>{
-    console.log(`DB CONNECT THREE ${name}`);
+// ((name) =>{
+//     console.log(`DB CONNECT THREE ${name}`);
     
-})('hitesh');
+// })('hitesh');
 
-((name) => console.log (`DB CONNECT FIVE ${name}`))('hitesh')
+// ((name) => console.log (`DB CONNECT FIVE ${name}`))('hitesh')
+
+
+// let val1 = 5;
+// let val2 = 10;
+
+// function twoValue (num1,num2){
+//     let result = num1 * num2;
+//     return result
+// }
+
+// const total = twoValue(val1, val2);
+// const total2 = twoValue(10, 10);
+
+// console.log(total);
+// console.log(total2);
+
+
+// *********JAVASCRIPT EXECUTION CONTEXT******//
+
+
+
+var A = 20;
+var B = 30;
+
+function addValue(){
+    let myName = "md selim";
+    console.log(myName);
+    console.log('Hello Javascript');
+    console.log(A);
+    console.log(B);
+    if(true){
+         return  `its right value`
+     }
+     
+    }
+addValue()
---
+++ 01_Basics/20_controlFlow.js
@@ -0,0 +1,39 @@
+// Arith Matic Operator..
+
+// +,-,*,/ increment decrement exponentiaon, modulus//
+
+// let a = 20;
+// let b = 30;
+// b++
+// a--
+
+// console.log(b%a);
+
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+// Comparison Operator//
+
+// <,>,=<,>=, ==, !=,===
+
+const score = 50;
+
+if(score >= 100){
+    console.log('score is 200');
+    
+} else{
+    console.log(`score is ${score}`);
+    
+}
+let temperature = 45
+
+if (temperature >= 50){
+    console.log("dhaka temperature 55");
+    
+} else{
+    console.log("Dhaka temperature 45");
+    
+}
