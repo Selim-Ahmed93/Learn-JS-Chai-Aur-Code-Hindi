e2522a628e945ea25724c321c0cd7415e8e83c63
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2026-01-19 21:14:44 +0600

    condition flow done

--- 01_Basics/20_controlFlow.js
+++ 01_Basics/20_controlFlow.js
@@ -73,30 +73,92 @@
    
 // }
 
-// switch case 
+// switch case IN JAVASCRIPT //
 
-const month = 3;
+// const month = 'Apr';
 
-switch (month) {
-    case 1:
-        console.log('January');
+// switch (month) {
+//     case "Jan":
+//         console.log('January');
         
-        break;
-        case 2:
-            console.log('February');
-            break;
-            case 3:
-                console.log('March');
-                break;
-            case 4:
-                console.log('April');
-                 break;
-            case 5:
-              console.log('May');
-              break;
+//         break;
+//         case "feb":
+//             console.log('February');
+//             break;
+//             case "mar":
+//                 console.log('March');
+//                 break;
+//             case "Apr":
+//                 console.log('April');
+//                  break;
+//             case "may":
+//               console.log('May');
+//               break;
                         
-    default:
-        console.log(' this not a Month ');
+//     default:
+//         console.log(' this not a Month ');
         
-        break;
+//         break;
+// };
+
+// Truthy Value or false value in Javascript//
+// false Value in Js = False, 0n, NaN,Undefined,Null,BigInt,0,-0
+// Truthy VALUE IN JAVASCRIPT = "FALSE","[]" "{}" Function (){},
+
+
+// const userEmail = "0";
+
+// if(userEmail){
+//     console.log('Got User Email');
+   
+// } else{
+//     console.log('this not  a Email');
+    
+// }
+
+// && or || operator in js
+
+// && operator means all values ofcurse True //
+// OR Operator means only one values true then execute code or Ignored this code 
+
+// lets start ..
+
+let value1 = true;
+let value2 = false;
+
+if(value1 && value2){
+     console.log('Its Right Value');
+     
 }
+
+// this code not execute thats mean && this operator check all value are true //
+
+if(value1 || value2){
+     console.log('execute This value');
+     
+}
+
+// this  code yes run OR || operator check one value true then code Run Oky//
+
+// Nullish Coalescing Operator ?? // 
+
+let val;
+//    val = 10 ?? 55;
+
+//    val  = null ?? 10; 
+val = undefined ?? 10;
+
+//    console.log(val);
+   
+// Ternary Operator in JS //
+ // Ternary Operator lets Start  syntax 
+
+ // condition ? true : False ;
+
+ const unitPrice = 2000;
+
+ unitPrice >= 3500 ? console.log('You Can Buy It') : console.log('You Can Not Buy It');
+
+ const coffePrice = 780;
+ coffePrice >= 800 ? console.log('You Are Not Able') : console.log('You Are Able this time');
+ 
