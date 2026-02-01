ee6334f6d8bbd2c5dc755b04332dc42e782b9d8d
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2026-02-01 20:11:43 +0600

    loop in js

--- 01_Basics/21_loop.js
+++ 01_Basics/21_loop.js
@@ -1,15 +1,59 @@
 //  const myArr = ['apple', 'orange','banana','mango']
- for(let i = 0; i <= 10; i++) {
-    const element = i;
+//  for(let i = 0; i <= 10; i++) {
+//     const element = i;
  
   
   
 
- if(element== 5){
-    console.log('5 is best Number') 
- }
+//  if(element== 5){
+//     console.log('5 is best Number') 
+//  }
 
-    console.log(element);
+//     console.log(element);
     
     
-}
+// }
+
+//  Lets Start For Of LOOP IN JS//
+
+const myArr = [4,5,6,3,6,7];
+     for (const element of myArr) {
+        console.log(element);
+        if (element== 9) {
+         console.log('this is number 6');
+         
+        }
+        else{
+         console.log('this number no find');
+         
+        }
+     }
+
+     // For Of Loop in   objects //
+
+     const person = {
+            name:'selim ahmed',
+            address: 'Dhaka',
+            idCard:736353,
+            loggedIn: true,
+     }
+
+      for (const element of Object.keys(person)){
+      //   console.log(element);
+        
+     }
+
+     // MAP 
+     const map = new Map();
+     map.set('IN',"INDIA");
+     map.set('BD', "BANGLADESH");
+     map.set('USA',"United State Of America");
+
+   //   console.log(map);
+
+   for (const [key,value] of map) {
+      console.log(key,':-',value);
+      
+   }
+     
+     
--- 01_Basics/22_while_loop.js
+++ 01_Basics/22_while_loop.js
@@ -8,6 +8,25 @@ while (i <=10) {
 let score = 0;
 
 do {
-    console.log(`score is printed ${score}`);
+    // console.log(`score is printed ${score}`);
     score ++;
 } while (score <= 10);
+
+
+// even loop in js //
+
+console.log('start one');
+
+setTimeout(() =>{
+    console.log('setTime out call back');
+    
+});
+
+Promise.resolve().then(()=>{
+    console.log('Promise call back');
+    
+})
+
+console.log('End Program');
+
+
